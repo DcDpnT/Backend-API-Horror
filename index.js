e398c970d064870fff83cebb40ff2d500d42cf63
DcDpnT <dupont.d33720@gmail.com>
2023-06-22 14:31:13 +0200

    ajout Cors pour echange front/back

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require ("express");
+const cors = require ("cors")
 
 const port = 4242;
 
 const app = express();
+app.use(cors("*"))
 app.use(express.static('assets'));
 const homeData = [
     {
